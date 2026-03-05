a9100bd979a4227278493264e6f6ebf5c1a26659
hariatexe <ignacioariasexe@gmail.com>
2026-03-05 19:11:24 -0300

    Preparando archivos a rellenar

---
+++ introduccion-ejercitacion.py
@@ -0,0 +1,27 @@
+# EJERCICIOS VARIABLES:
+
+# 1. Pedir al usuario dos números y mostrar la suma, resta, multiplicación y división.
+# 2. Pedir el nombre del usuario y salúdalo con "Hola, {nombre}".
+# 3. Implementar un código que verifique si el input introducido es un booleano.
+
+#=================================================================================================#
+
+# EJERCICIOS CONDICIONALES:
+
+# 1. Pedir un número y determinar si es positivo, negativo o cero.
+# 2. Calcular la nota de un estudiante (0-100) y mostrar la letra correspondiente.
+# A (>= 90), B (>= 80), C (>= 70), F (< 70)
+# 3. Pedir al usuario dos números y mostrar cuál es mayor o si son iguales.
+# 4. Implementar un Switch.
+
+#=================================================================================================#
+
+# EJERCICIOS BUCLES:]
+
+# 1. Imprimir los números del 1 al 20 usando for.
+# 2. Imprimir los numeros pares del 2 al 50
+# 3. Sumar todos los numeros de una lista usando for.
+# 4. Pedir números al usuario hasta que escriba 0, y muestra la suma suma total (while).
+# 5. Usar enumerate() en una lista de nombres y mostrar indice + nombre.
+
+#=================================================================================================#
